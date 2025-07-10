7967720a2f30582b9a960e6471e89196d6f5bf71
James Harbin <james.harbin@york.ac.uk>
2025-07-10 16:42:39 +0100

    New datasets

--- run_experiments.py
+++ run_experiments.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     #run_all_algs.run_all_algs_on_dataset(datasets.expt_config_turtlebot_multi_tb1_server_allops, using_inceptiontime = False)
 
     # TODO: update ETERRY when new run finishes
-    run_all_algs.run_all_algs_on_dataset(datasets.expt_config_eterry_human1_1100, using_inceptiontime=False)
-    run_all_algs.run_all_algs_on_dataset(datasets.expt_config_eterry_statichumans_1100, using_inceptiontime = False)
-    run_all_algs.run_all_algs_on_dataset(datasets.expt_config_eterry_pathcompletion_1100, using_inceptiontime = False)
+    #run_all_algs.run_all_algs_on_dataset(datasets.expt_config_eterry_human1_15files, using_inceptiontime=False)
+    #run_all_algs.run_all_algs_on_dataset(datasets.expt_config_eterry_statichumans_15files, using_inceptiontime = False)
+    run_all_algs.run_all_algs_on_dataset(datasets.expt_config_eterry_pathcompletion_15files, using_inceptiontime = False)
