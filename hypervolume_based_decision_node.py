e0578e7ea4655e1ea78e39070d262d8452a7e6c1
James Harbin <james.harbin@york.ac.uk>
2025-09-03 07:14:00 +0100

    Fixes to hypervolume decision nodes

--- hypervolume_based_decision_node.py
+++ hypervolume_based_decision_node.py
@@ -7,19 +7,19 @@ import pandas as pd
 from decision_node import DecisionNode, log
 from indicator_based_decision_node import IndicatorBasedDecisions
 
+class GDCalculationFailed(Exception):
+    """Error in GD calculation"""
+
 class HypervolumeWithGDRelativeQuality(IndicatorBasedDecisions):
     def extract_point_from_front_df(self, front_df, point_index):
-        if point_index < len(front_df):
-            extracted_point_df = front_df.iloc[[point_index]]
-            front_df_copy = front_df.copy()
-            front_without_point_df = front_df_copy.drop(point_index).reset_index(drop=True)
-            return extracted_point_df, front_without_point_df
-        else:
-            return None, None
+        front_df_copy = front_df.copy().reset_index()
+        extracted_point_df = front_df.iloc[[point_index]]
+        front_without_point_df = front_df_copy.drop(point_index)
+        return extracted_point_df, front_without_point_df
 
     def get_all_gds_for_existing_front_points(self, front_df):
         gds = []
-        for point_index, _ in front_df.iterrows():
+        for point_index in range(0, len(front_df)):
             point_df, front_without_point_df = self.extract_point_from_front_df(front_df, point_index)
             # if the front is empty, there will be no GD for this point
             if point_df is not None:
@@ -31,12 +31,20 @@ class HypervolumeWithGDRelativeQuality(IndicatorBasedDecisions):
         current_best_front_df = pd.DataFrame(self.current_best_front)
         predicted_test_metrics_df = pd.DataFrame(predicted_test_metrics)
 
-        gd_for_hypothetical_point = self.decision_analysis.gd_for_point(current_best_front_df, predicted_test_metrics_df)
-        other_gds = self.get_all_gds_for_existing_front_points(current_best_front_df)
+        if len(current_best_front_df) > 1:
+            gd_for_hypothetical_point = self.decision_analysis.gd_for_point(current_best_front_df, predicted_test_metrics_df)
+            other_gds = self.get_all_gds_for_existing_front_points(current_best_front_df)
 
-        if len(other_gds) > 0:
+            # sanity check: there should be as many GDs as there are points in the front!
+            if len(other_gds) < len(current_best_front_df):
+                print(f"len(other_gds) = {len(other_gds)}, len(current_best_front_df)={len(current_best_front_df)}")
+                raise GDCalculationFailed
+
             rq = 1.0 - gd_for_hypothetical_point / np.max(other_gds)
+            log.debug(f"predicted_test_metrics={predicted_test_metrics},gd_for_new_point={gd_for_hypothetical_point}, other_gds={other_gds}, rq={rq}")
         else:
+            # if the front is too small (zero or one element) always give relative quality of 1 to new element
             rq = 1.0
-        print(f"predicted_test_metrics={predicted_test_metrics},gd_for_new_point={gd_for_hypothetical_point}, other_gds={other_gds}, rq={rq}")
+            log.debug(f"Front is too small (zero or one element) - using relative_quality = 1.0")
+
         return rq
